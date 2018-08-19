280e1afe0dd8b397d7e21921e62c3f4cb6da8555
Nelson <nlrarojas@hotmail.com>
2018-08-18 19:11:41 -0600

    Creación del proyecto

---
+++ src/main/Program.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import view.MainView;
+
+/**
+ *
+ * @author Nelson
+ */
+public class Program {
+    public static void main (String [] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainView().setVisible(true);
+            }
+        });
+    }
+}
