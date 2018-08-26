ad92a1ac6cbd0211e24390b27ea2a00d54eed623
Nelson <nlrarojas@hotmail.com>
2018-08-25 18:37:25 -0600

    Restructurando cambios

--- src/utility/Constants.java
+++ src/utility/Constants.java
@@ -1,6 +1,6 @@
 package utility;
 
-import javax.swing.JPanel;
+import core.Figure;
 
 /**
  *
@@ -8,16 +8,18 @@ import javax.swing.JPanel;
  */
 public class Constants implements IConstants{
     public static Constants constants;    
-    public static JPanel[] TRACKS;
+    public static Figure[] TRACKS;
+    public static int TRACK_WIDTH = 600/NUMBER_OF_TRACKS;
+    public static int HEIGHT = 600;
     
     public Constants () {
-        TRACKS = new JPanel[NUMBER_OF_TRACKS];
+        TRACKS = new Figure[NUMBER_OF_TRACKS];
         for (int i = 0; i < NUMBER_OF_TRACKS; i++) {
-            TRACKS[i] = new JPanel();            
+            TRACKS[i] = new Figure(TRACK_WIDTH, HEIGHT, i);            
         }
     }
     
-    public Constants getInstance() {
+    public static Constants getInstance() {
         if (constants == null) {
             return new Constants();
         }
